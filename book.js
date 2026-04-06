d7c2b75aa8593cbf6d98ef048daad89557a7e576
rrolonNelson <rrolon@nelson-atkins.org>
2026-04-06 17:25:56 -0500

    adjusting page 14

--- book.js
+++ book.js
@@ -1,56 +1,64 @@
 document.addEventListener('DOMContentLoaded', function() {
-     
-    document.querySelector(".container").classList.remove("hidden");
 
     const pageFlip = new St.PageFlip(
         document.getElementById("Book"),
         {
-            width: 500, // base page width
-            height: 600, // base page height
+            width: 550, // base page width
+            height: 733, // base page height
 
             size: "stretch",
             // set threshold values:
-            minWidth: 400,
+            minWidth: 315,
             maxWidth: 1200,
-
-            minHeight: 300,
+            minHeight: 420,
             maxHeight: 1200,
             
             flippingTime: 500,
             swipeDistance: 30,
-            disableFlipByClick: true,//false: turn page by clicking anywhere on page 
+//          disableFlipByClick: true,//false: turn page by clicking anywhere on page 
             maxShadowOpacity: 0.5, // shadow intensity
             showCover: false, //false: keeps book open
             mobileScrollSupport:false,// disable content scrolling on mobile devices
             // usePortrait: true,
+    
         }
     );
 
     // load pages
     pageFlip.loadFromHTML(document.querySelectorAll(".page"));
 
-    document.querySelector(".page-total").innerText = pageFlip.getPageCount()-1;
+    document.querySelector(".page-total").innerText = pageFlip.getPageCount();
     document.querySelector(".page-orientation").innerText = pageFlip.getOrientation();
 
-    // prev/nex button function
-   document.querySelector(".btn-prev").addEventListener("click", () => {
-    // console.log(pageFlip.getOrientation())
-      pageFlip.turnToPrevPage();
-      if (pageFlip.getOrientation() == 'landscape') {
-                pageFlip.flipPrev(); // Turn to the previous page (with animation)
-            } else {
-                pageFlip.turnToPrevPage();
-            }
-       
+    // prev/next button function
+    document.querySelector(".btn-prev").addEventListener("click", () => {
+        if (pageFlip.getOrientation() == 'portrait') {
+            pageFlip.turnToPrevPage();
+        } else {
+            pageFlip.flipPrev(); // Turn to the previous page (with animation)
+        }
     });
 
     document.querySelector(".btn-next").addEventListener("click", () => {
         pageFlip.flipNext(); // Turn to the next page (with animation)
     });
 
+    // home/close button function
+    const home = document.querySelector("#home-btn");
+    const close = document.querySelector("#close-btn");
+
+    // Event Listener
+    home.addEventListener("click", closeBook);
+    close.addEventListener("click", closeBook);
+
+    function closeBook(){
+        while(document.querySelector(".page-current").innerText > 1){
+        pageFlip.flipPrev();
+        }
+    }
+
     // triggered by page turning
     pageFlip.on("flip", (e) => {
-        console.log("page flip")
         document.querySelector(".page-current").innerText = e.data + 1;
     });
 
@@ -63,45 +71,5 @@ document.addEventListener('DOMContentLoaded', function() {
     pageFlip.on("changeOrientation", (e) => {
         document.querySelector(".page-orientation").innerText = e.data;
     });
-
-
-    function closeBook(e){
-        let count = document.querySelector(".page-current").innerText;
-        let pageNum = parseInt(count);
-        console.log(parseInt(count));
- 
-        while (pageNum > 0) {
-        //    console.log(document.querySelector(".page-current").innerText);
-        //    console.log(pageFlip.getCurrentPageIndex());
-            if (pageFlip.getOrientation() == 'landscape') {
-                // console.log(pageFlip.getOrientation())
-                pageFlip.flipPrev();
-                console.log(pageNum);
-            } else if (pageFlip.getOrientation() == 'portrait')  {
-                //  console.log(pageFlip.getOrientation())
-                pageFlip.turnToPrevPage();
-                console.log(pageNum);
-            }
-        
-    
-
-        // console.log(document.querySelector(".page-current").innerText);
-        // console.log(pageFlip.getCurrentPageIndex());
-        
-
-        }
-    };
-
-    // home button function
-    const home = document.querySelector("#home-btn");
-    const close = document.querySelector("#close-btn");
-
-    // Event Listener
-    home.addEventListener("click", closeBook);
-    close.addEventListener("click", closeBook);
-
-
-
-
 });
 
