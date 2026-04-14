23b0b1a03f546162f2fe1ee0793de816393ee4b9
rrolon <rrolon@nelson-atkins.org>
2026-04-14 16:43:39 -0500

    trying to learn the gtag connection

--- cookies.js
+++ cookies.js
@@ -63,3 +63,82 @@ cookieMessage = () => {
 window.addEventListener("load", cookieMessage);
 
 
+// const setDefaultConsentState = require("setDefaultConsentState");
+// const gtagSet = require("gtagSet");
+// const getCookieValues = require("getCookieValues");
+// const updateConsentState = require("updateConsentState");
+
+// let setDefaultSetting = true;
+// const regionSettings = data.regionSettings || [];
+// const waitForTime = data.waitForTime;
+
+// function getConsentStateForCategory(categoryConsent) {
+//   return categoryConsent === "yes" ? "granted" : "denied";
+// }
+
+// function setConsentInitStates(consentData) {
+//   if (waitForTime > 0) consentData.wait_for_update = waitForTime;
+//   setDefaultConsentState(consentData);
+// }
+
+// gtagSet({
+//   ads_data_redaction: !!data.adsRedaction,
+//   url_passthrough: !!data.urlPassThrough,
+//   "developer_id.dZDk4Nz": true,
+// });
+
+// for (let index = 0; index < regionSettings.length; index++) {
+//   const regionSetting = regionSettings[index];
+//   const consentRegionData = {
+//     ad_storage: regionSetting.advertisement,
+//     analytics_storage: regionSetting.analytics,
+//     functionality_storage: regionSetting.functional,
+//     personalization_storage: regionSetting.functional,
+//     security_storage: regionSetting.security,
+//     ad_user_data: regionSetting.adUserData,
+//     ad_personalization: regionSetting.adPersonal
+//   };
+//   const regionsToSetFor = regionSetting.regions
+//     .split(",")
+//     .map((region) => region.trim())
+//     .filter((region) => region);
+//   if (regionsToSetFor.length > 0 && regionsToSetFor[0].toLowerCase() !== "all")
+//     consentRegionData.region = regionsToSetFor;
+//   else setDefaultSetting = false;
+//   setConsentInitStates(consentRegionData);
+// }
+
+// if (setDefaultSetting) {
+//   setConsentInitStates({
+//     ad_storage: "denied",
+//     analytics_storage: "denied",
+//     functionality_storage: "denied",
+//     personalization_storage: "denied",
+//     security_storage: "granted",
+//     ad_user_data: "denied",
+//     ad_personalization: "denied"
+//   });
+// }
+
+// const consentString = getCookieValues("wt_consent", false)[0];
+// console.log(consentString);
+// if (consentString && typeof consentString === "string") {
+//   const cookieObj = consentString.split(",").reduce(function (acc, curr) {
+//     const cookieValue = curr.trim().split(":");
+//     acc[cookieValue[0]] = getConsentStateForCategory(cookieValue[1]);
+//     return acc;
+//   }, {});
+
+//   updateConsentState({
+//     ad_storage: cookieObj.advertisement,
+//     analytics_storage: cookieObj.analytics,
+//     functionality_storage: cookieObj.functional,
+//     personalization_storage: cookieObj.functional,
+//     security_storage: cookieObj.necessary,
+//     ad_user_data: cookieObj.advertisement,
+//     ad_personalization: cookieObj.advertisement,
+//   });
+// }
+
+// // Success callback
+// data.gtmOnSuccess();
