e229a6ccef1d52e32d70f6359a4cdafb4aebaa39
rrolon <rrolon@nelson-atkins.org>
2026-08-21 12:04:15 -0500

    getting controlls and overlays to work so clicks dont open other books.  Need to figure out click to open area

--- book.js
+++ book.js
@@ -8,43 +8,11 @@ books.forEach(book => {
     bookWrapper.innerHTML = ` ${book.pages.map(page => ` 
                     <div class=" page page-content">
                         <img id="${page.imgId}" src="${page.imgSrc}" alt="${page.imgAlt}" ></img>
-                        
                     </div>
                     `)
                 .join("")}`;
 
     bookContainer.appendChild(bookWrapper);
-
-    const prevBtn = document.querySelector(".btn-prev");
-    const nextBtn = document.querySelector(".btn-next");
-
-    nextBtn.innerHTML = `
-        <!-- Next Button -->
-        <button type="button" class="btn-next hidden" id="next-btn-${book.id}">
-            <i class="fas fa-chevron-right"></i>
-            <p>Next</p>
-        </button>`;
-
-    prevBtn.innerHTML = `
-        <!-- Previous Button -->
-        <button type="button" class="btn-prev hidden" id="prev-btn-${book.id}" >
-            <i class="fas fa-chevron-left"></i>
-            <p>Previous</p>
-        </button>`;
-
-    const homeBtn = document.querySelector(".home-btn");
-
-    homeBtn.innerHTML = `
-    <button id="home-btn-${book.id}"  class="hidden">
-        <i class="fas fa-book"></i>
-        <br>
-        <p>Close Book</p>
-        <p>[<span class="page-current" id="page-current-${book.id}">1</span> of <span class="page-total" id="page-total-${book.id}>-</span>]</p>
-    </button>
-    <div style=" display: none; visibility: hidden;">
-        State: <i class="page-state" id="page-state-${book.id}">read</i>, orientation: <i class="page-orientation" id="page-orientation-${book.id}">landscape</i>
-    </div>
-    `;
         
 });
 
@@ -54,103 +22,128 @@ function createBook(bookData) {
     const element = document.getElementById(bookData.id);
     pageFlip = new St.PageFlip(element, 
         {
-        width:800, // base page width
-        height:1000, // base page height
+            width:800, // base page width
+            height:1000, // base page height
+            usePortrait: false,
+            // minWidth: 200, 
+            // maxWidth: 1000, 
+            // minHeight: 200, 
+            // maxHeight: 1000,
 
-        // size: "fixed",
-        
-        flippingTime: 500,
-        swipeDistance: 30,
-        disableFlipByClick: true,//false: turn page by clicking anywhere on page 
-        maxShadowOpacity: 0.5, // Half shadow intensity
-        showCover: true, //false: keeps book open
-        mobileScrollSupport: false // disable content scrolling on mobile devices
-    });
-
-    pageFlip.loadFromHTML(
-        element.querySelectorAll(".page")
-    );
-
-    document.addEventListener('DOMContentLoaded', function() {
-        console.log("loaded")
-
-        console.log( document.getElementById(`btn-prev-${bookData.id}`))
-        document.getElementById(`prev-btn-${bookData.id}`).addEventListener("click", () => {
-            pageFlip.flipPrev(); // Turn to the previous page (with animation)
+            // size: "stretch",
+            
+            // flippingTime: 500,
+            // swipeDistance: 30,
+            maxShadowOpacity: 0.5, // Half shadow intensity
+            showCover: true, //false: keeps book open
+            mobileScrollSupport: false // disable content scrolling on mobile devices
         });
 
-        document.getElementById(`next-btn-${bookData.id}`).addEventListener("click", () => {
-            pageFlip.flipNext(); // Turn to the next page (with animation)
-        });
+    pageFlip.loadFromHTML( element.querySelectorAll(".page") );
 
-        pageFlip.on("flip", e => {
-            // triggered by page turning
-            document.getElementById(`page-current-${bookData.id}`).innerText = e.data + 1;
-            console.log(`Book: ${bookData.id}`);
-            console.log(`Page: ${e.data}`);
-        });
-
-        pageFlip.on("changeState", e => {
-            // triggered when the state of the book changes
-            document.getElementById(`page-state-${bookData.id}`).innerText = e.data;
-        });
-
-        pageFlip.on("changeOrientation", e => {
-            // triggered when page orientation changes
-            document.getElementById(`page-orientation-${bookData.id}`).innerText = e.data;
-
-        });
-
-        document.getElementById(`page-total-${book.id}`).innerText = pageFlip.getPageCount()-1;
-        document.getElementById(`page-orientation-${bookData.id}`).innerText = pageFlip.getOrientation();
-
-        
-        // home button function
-        const home = document.getElementById(`home-btn-${book.id}`)
-
-        // Event Listener
-        home.addEventListener("click", function (){
-            while(document.getElementById(`page-current-${bookData.id}`).innerText > 1){
-            pageFlip.flipPrev();
-            }
-        });
-    });
-    
     return pageFlip;
 }
 
 //Create books
 const pageFlips = {};
+let activeBook = null;
 books.forEach(book => {
     pageFlips[book.id] = createBook(book);
 });
-
-
+document.querySelector(".container").classList.remove("hidden");
 
 let flipbooks = document.querySelectorAll('.flip-book');
-for (let book of flipbooks) {
-	book.onclick = function(e) {
-        console.log("click triggered");
-        if (e.target.classList.contains('opened')) { 
-			e.target.classList.remove('opened');
-            e.target.classList.add('closed');
-            // get the id of the book and add or remove it to the buttons
-        }
-        else { 
-            e.target.classList.remove('closed');
-            e.target.classList.add('opened');
-            let homeBtn = document.getElementById(`home-btn-${book.id}`)
-            let prevBtn = document.getElementById(`prev-btn-${book.id}`)
-            let nextBtn = document.getElementById(`next-btn-${book.id}`)
-            console.log(prevBtn.classList)
-            homeBtn.classList.remove('hidden')
-            prevBtn.classList.remove('hidden')
-            nextBtn.classList.remove('hidden')
-            // show buttons for book navigation
-        }
-	}
-}
+let homeBtn = document.querySelector(`.home-btn`)
+let prevBtn = document.getElementById(`prev-btn`)
+let nextBtn = document.getElementById(`next-btn`)
+let overlay = document.getElementById("overlay")
 
+//click listeners 
+bookContainer.addEventListener('click', (e) => {
+    let openedBook = e.target.closest('.flip-book');
+    if (!openedBook) return;
+
+    flipbooks.forEach(book => {
+            console.log("click triggered");
+            //if book clicked is not active book close book??
+            if (book === openedBook) {
+                    //open book and set which book is active
+                book.classList.remove('closed');
+                book.classList.add('opened');
+                book.classList.add('bring-to-front');
+                activeBook = pageFlips[book.id];
+
+                //show controlls
+                homeBtn.classList.add('bring-to-front')
+                prevBtn.classList.add('bring-to-front')
+                nextBtn.classList.add('bring-to-front')
+                overlay.classList.remove('hidden')
+                homeBtn.classList.remove('hidden')
+                prevBtn.classList.remove('hidden')
+                nextBtn.classList.remove('hidden')
+            
+                //update state of book
+                activeBook.on("flip", e => {
+                    // triggered by page turning
+                    document.getElementById(`page-current`).innerText = e.data + 1;
+                    // console.log(`Book: ${activeBook.id}`);
+                    // console.log(`Page: ${e.data}`);
+                    // console.log(`page total: ${activeBook?.getPageCount()}`);
+                });
+
+                activeBook.on("changeState", e => {
+                    // triggered when the state of the book changes
+                    document.getElementById(`page-state`).innerText = e.data;
+                });
+
+                activeBook.on("changeOrientation", e => {
+                    // triggered when page orientation changes
+                    document.getElementById(`page-orientation`).innerText = e.data;
+
+                });
+
+                document.getElementById(`page-total`).innerText = activeBook.getPageCount()-1;
+                document.getElementById(`page-orientation`).innerText = activeBook.getOrientation();
+            } else {
+                book.classList.add('hidden')
+            }  
+    })
+})
+
+
+document.getElementById(`next-btn`).addEventListener("click", () => {
+    activeBook?.flipNext();
+});
+
+ document.getElementById(`prev-btn`).addEventListener("click", () => {
+    activeBook?.flipPrev();
+}); 
+
+document.getElementById(`home-btn`).addEventListener("click", () => {
+    while(document.getElementById(`page-current`).innerText > 1){
+        activeBook?.flipPrev();
+    }
+
+    flipbooks.forEach(book => {
+        if ( book.classList.contains('opened')){
+            book.classList.remove('opened');
+            book.classList.remove('bring-to-front')
+            book.classList.add('closed');
+            homeBtn.classList.remove('bring-to-front')
+            prevBtn.classList.remove('bring-to-front')
+            nextBtn.classList.remove('bring-to-front')
+            homeBtn.classList.add('hidden')
+            prevBtn.classList.add('hidden')
+            nextBtn.classList.add('hidden')
+            overlay.classList.add('hidden')
+        } else if (book.classList.contains('closed')){
+            book.classList.remove('hidden')
+        }
+    })
+
+    activeBook = null;
+
+});
 
 
 
